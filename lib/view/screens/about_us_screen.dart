56a0d84dea768832bcf17a372a71a5d2bd77fe7a
ahmedsaed96 <61468429+ahmedsaed96@users.noreply.github.com>
2021-08-02 08:52:06 +0200

    finsh about us

--- lib/view/screens/about_us_screen.dart
+++ lib/view/screens/about_us_screen.dart
@@ -1,4 +1,8 @@
+import 'package:clipboard/clipboard.dart';
+import 'package:cocking_time/constant.dart';
+import 'package:cocking_time/local/shared_prefrences.dart';
 import 'package:cocking_time/view/widgets/custom_drower.dart';
+import 'package:cocking_time/view/widgets/functions.dart';
 import 'package:flutter/material.dart';
 
 class AboutUsScreen extends StatelessWidget {
@@ -6,9 +10,42 @@ class AboutUsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: buildAppBar(appBarTittle: 'About us'),
       drawer: CustomDrawer(),
       body: SafeArea(
-        child: Column(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'ahmedsaednegm96@gmail.com',
+                  style: googleStyleSmall(context),
+                ),
+                const SizedBox(width: 10.0),
+                IconButton(
+                    onPressed: () {
+                      FlutterClipboard.copy('ahmedsaednegm96@gmail.com')
+                          .then((value) {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text('Copied'),
+                        ));
+                      }).catchError((e) {
+                        debugPrint('erorr cant copy the Email $e');
+                      });
+                    },
+                    icon: Icon(
+                      Icons.copy,
+                      color: MySharedPrefrences.getString(key: 'mode') == 'true'
+                          ? Colors.black
+                          : Colors.white,
+                    )),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
